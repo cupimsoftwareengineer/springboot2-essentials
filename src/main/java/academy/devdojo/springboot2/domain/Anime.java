133b6bea93adc14cbb2995fd955f9af281d8801b
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2023-01-10 20:47:55 -0300

    15 - Framework de Mapeamento MapStruct

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,13 +1,14 @@
 package academy.devdojo.springboot2.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -4,4 +4,5 @@ import academy.devdojo.springboot2.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
 }
