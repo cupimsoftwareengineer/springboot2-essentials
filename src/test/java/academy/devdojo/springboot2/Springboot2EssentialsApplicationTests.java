153077c8c0ba8fa39114654158d9ba326cc6eba0
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2023-01-15 16:03:02 -0300

    38 - Maven Profile Para Testes de Integração

--- src/test/java/academy/devdojo/springboot2/Springboot2EssentialsApplicationTests.java
+++ src/test/java/academy/devdojo/springboot2/Springboot2EssentialsApplicationTests.java
@@ -3,11 +3,11 @@ package academy.devdojo.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Springboot2EssentialsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //	@Test
+    void contextLoads() {
+    }
 
 }
