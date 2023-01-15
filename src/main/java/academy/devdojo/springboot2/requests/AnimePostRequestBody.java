326c849ea7b68fdf03cbe3285455492084ec986d
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2023-01-15 15:17:25 -0300

    34 - Unit Tests pt 02

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,11 +1,12 @@
 package academy.devdojo.springboot2.requests;
 
+import lombok.Builder;
 import lombok.Data;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
